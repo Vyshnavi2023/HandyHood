09fc1b316fcb7ffebb92559bb27ad3e2dcbcd39d
vyshnavi sangani <ssanganivaishnavi@gmail.com>
2026-02-02 11:30:00 +0530

    Implemented database connection and user registration page using PHP and MySQL

--- config/database.php
+++ config/database.php
@@ -4,6 +4,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $client = new MongoDB\Client("mongodb://localhost:27017");
 
-// ⚠ Use lowercase (because Linux is case sensitive)
+// ⚠ Use lowercase (because Linux is case sensitive) 
+// data base connection added
 $db = $client->handyhood;
 ?>
